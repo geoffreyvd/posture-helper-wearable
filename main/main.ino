4e1c6883d6771578a313eca9f634baf00036315f
geoffrey <geoffrey.v.d@live.nl>
2017-11-11 18:25:31 +0100

    example

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,12 @@
 #include "FlexSensor.h"
+
+FlexSensor flex1(13);
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  flex1.read();
 }
